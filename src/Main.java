7f0dabf2400e1492f746b0073df335d7568723ee
Gid05 <gideonnega@gmail.com>
2025-06-18 20:40:52 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World2!");
         int count = 0;
         try {
             while (true) {
